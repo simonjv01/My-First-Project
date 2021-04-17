f2f8f619d36e437fcb58075a235ca54609d80032
Simon Vargas <sjvargas01@gmail.com>
2021-04-17 09:35:30 -0400

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [user, setUser] = useState("Simon")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.mystyle}>{user}</Text>
+      <View style={styles.buttonstyle}><Button title="Change"/></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mystyle:{
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 40,
+    backgroundColor: 'yellow',
+  },
+  buttonstyle:{
+    marginTop: 30,
+  }
 });
