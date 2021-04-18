35916dd789db0feca7309f55c902520b8278803a
Simon Vargas <sjvargas01@gmail.com>
2021-04-18 16:53:47 -0400

    updated App.js file

--- App.js
+++ App.js
@@ -1,32 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 
 export default function App() {
-  const [user, setUser] = useState("Simon")
+const [todos, setTodos] = useState([{
+  
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  },
+])
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.mystyle}>{user}</Text>
-      <View style={styles.buttonstyle}><Button title="Change"/></View>
-      <StatusBar style="auto" />
+      {todos.map(todo=>{
+        return (<View key={todo.id} style={styles.todo}>
+          <Text>{todo.title}</Text>
+        </View>)
+
+
+      })}
+     
+     
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  mystyle:{
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 40,
-    backgroundColor: 'yellow',
-  },
-  buttonstyle:{
-    marginTop: 30,
+  todo: {
+    margin: 30,
+    backgroundColor: 'deeppink',
+    color: 'white',
+    fontSize: 30,
+    padding: 30,
   }
+  
 });
